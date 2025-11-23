4822d8ff77f368b7f3201e7460571f0ee3528fd0
Francesco <francesco-lion@virgilio.it>
2025-11-23 16:00:28 +0100

    chore: constants mongo file

---
+++ src/consts.mongo.ts
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const MongoConnectionString = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`;
+export const MongoUsersCollection = 'users';
