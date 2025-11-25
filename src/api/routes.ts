23583f9bb5e4c0f96ca92af7f92df10bee003e6f
Francesco <francesco-lion@virgilio.it>
2025-11-25 15:38:03 +0100

    chore: error handling

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -14,12 +14,16 @@ const router = Router();
 const db = new MongoService();
 
 router.post('/login', UserAlreadyAuthenticated, async (req, res) => {
-  const { userName, pw } = req.body;
-  const users = db.getCollection(MongoUsersCollection);
-  const user = await users.findOne({ userName });
-  if (!user || !bcrypt.compareSync(pw, user.pw)) return res.status(403).json({ message: 'login failed' });
-  const token = jwt.sign({ userName, sub: user._id }, process.env.JWT_ACCESS_TOKEN ?? '', { expiresIn: '1h' });
-  res.status(201).json({ token });
+  try {
+    const { userName, pw } = req.body;
+    const users = db.getCollection(MongoUsersCollection);
+    const user = await users.findOne({ userName });
+    if (!user || !bcrypt.compareSync(pw, user.pw)) return res.status(403).json({ message: 'login failed' });
+    const token = jwt.sign({ userName, sub: user._id }, process.env.JWT_ACCESS_TOKEN ?? '', { expiresIn: '1h' });
+    res.status(201).json({ token });
+  } catch (e: any) {
+    res.status(500).json({ message: e.message })
+  }
 });
 
 router.post('/register', AssertBodyParameters, async (req, res) => {
@@ -39,47 +43,63 @@ router.post('/register', AssertBodyParameters, async (req, res) => {
 });
 
 router.post('/createTask', VerifyAuthentication, async (req, res) => {
-  const { sub: userId } = (req as any).user;
-  const { name, description } = req.body;
-  if (!name || typeof name !== 'string') return res.status(400).json({ message: 'name should be a valid string' });
-  if (!description || typeof name !== 'string') return res.status(400).json({ message: 'description should be a valid string' });
-  const tasks = db.getCollection(MongoTasksCollection);
-  const data = { userId: new ObjectId(userId), name, description, status: 0 };
-  await tasks.insertOne(data);
-  res.status(201).json(data);
+  try {
+    const { sub: userId } = (req as any).user;
+    const { name, description } = req.body;
+    if (!name || typeof name !== 'string') return res.status(400).json({ message: 'name should be a valid string' });
+    if (!description || typeof name !== 'string') return res.status(400).json({ message: 'description should be a valid string' });
+    const tasks = db.getCollection(MongoTasksCollection);
+    const data = { userId: new ObjectId(userId), name, description, status: 0 };
+    await tasks.insertOne(data);
+    res.status(201).json(data);
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 router.get('/getTasks', VerifyAuthentication, async (req, res) => {
-  const { sub: userId } = (req as any).user;
-  const tasks = db.getCollection(MongoTasksCollection);
-  const doc = await tasks.find({ userId: new ObjectId(userId) }).toArray();
-  res.status(200).json(doc);
+  try {
+    const { sub: userId } = (req as any).user;
+    const tasks = db.getCollection(MongoTasksCollection);
+    const doc = await tasks.find({ userId: new ObjectId(userId) }).toArray();
+    res.status(200).json(doc);
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 router.delete('/deleteTask', VerifyAuthentication, async (req, res) => {
-  const { taskId } = req.body;
-  if (!taskId) return res.status(400).json({ message: 'taskId cannot be null' });
-  const tasks = db.getCollection(MongoTasksCollection);
-  const taskById = await tasks.findOne({ _id: new ObjectId(taskId) });
-  if (!taskById) return res.status(404).json({ message: 'task not found' });
-  await tasks.deleteOne({ _id: new ObjectId(taskId) });
-  res.status(200).json({ message: 'task deleted' });
+  try {
+    const { taskId } = req.body;
+    if (!taskId) return res.status(400).json({ message: 'taskId cannot be null' });
+    const tasks = db.getCollection(MongoTasksCollection);
+    const taskById = await tasks.findOne({ _id: new ObjectId(taskId) });
+    if (!taskById) return res.status(404).json({ message: 'task not found' });
+    await tasks.deleteOne({ _id: new ObjectId(taskId) });
+    res.status(200).json({ message: 'task deleted' });
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 router.put('/changeTask', VerifyAuthentication, async (req, res) => {
-  const { taskId, name, description, status } = req.body;
-  if (!taskId) return res.status(400).json({ message: 'taskId cannot be null' });
-  if (name && typeof name !== 'string') return res.status(400).json({ message: 'name should be a valid string' });
-  if (description && typeof description !== 'string') return res.status(400).json({ message: 'description should be a valid string' });
-  if (status !== 0 && status !== 1) return res.status(400).json({ message: 'status should be 0 (pregress) or 1 (done)' });
-  const tasks = db.getCollection(MongoTasksCollection);
-  const taskById = await tasks.findOne({ _id: new ObjectId(taskId) });
-  if (!taskById) return res.status(404).json({ message: 'task not found' });
-  const data: any = { status };
-  if (name) data.name = name;
-  if (description) data.description = description;
-  await tasks.updateOne({ _id: new ObjectId(taskId) }, { $set: data });
-  res.status(200).json({ message: 'task updated' });
+  try {
+    const { taskId, name, description, status } = req.body;
+    if (!taskId) return res.status(400).json({ message: 'taskId cannot be null' });
+    if (name && typeof name !== 'string') return res.status(400).json({ message: 'name should be a valid string' });
+    if (description && typeof description !== 'string') return res.status(400).json({ message: 'description should be a valid string' });
+    if (status !== 0 && status !== 1) return res.status(400).json({ message: 'status should be 0 (pregress) or 1 (done)' });
+    const tasks = db.getCollection(MongoTasksCollection);
+    const taskById = await tasks.findOne({ _id: new ObjectId(taskId) });
+    if (!taskById) return res.status(404).json({ message: 'task not found' });
+    const data: any = { status };
+    if (name) data.name = name;
+    if (description) data.description = description;
+    await tasks.updateOne({ _id: new ObjectId(taskId) }, { $set: data });
+    res.status(200).json({ message: 'task updated' });
+  } catch (e: any) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 export default router;
